314ec83358666c6fa834fbc21282b05c2965df3c
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-08-27 14:00:32 +0100

    privacy included

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\APIAddressController;
 use App\Http\Controllers\APIAuthController;
-
 use App\Http\Controllers\APIBannerController;
 use App\Http\Controllers\APIBrandController;
 use App\Http\Controllers\APICategoryController;
@@ -12,13 +11,10 @@ use App\Http\Controllers\APIProductReviewController;
 use App\Http\Controllers\APISettingsController;
 use App\Http\Controllers\APIUploadController;
 use App\Http\Controllers\APIUserController;
+use App\Http\Controllers\PrivacyPolicyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
-
-
-
-
 
 // Authentication Routes
 Route::post('/register', [APIAuthController::class, 'register']);
@@ -28,76 +24,69 @@ Route::post('/password/email', [APIAuthController::class, 'sendPasswordResetEmai
 Route::post('/password/reset', [APIAuthController::class, 'resetPassword']);
 Route::get('/email/verify/{id}/{hash}', [APIAuthController::class, 'verifyEmail'])->middleware(['throttle:6,1'])->name('verification.verify');
 
-    // Public Routes
-    Route::get('/categories', [APICategoryController::class, 'index']);
+// Public Routes
+Route::get('/categories', [APICategoryController::class, 'index']);
 
-    Route::get('/brands', [APIBrandController::class, 'index']);
-    Route::get('/brands/{id}', [APIBrandController::class, 'show']);
-    Route::get('/brands/isFeatured', [APIBrandController::class, 'featured']);
-    Route::get('/brands/category/{categoryId}', [APIBrandController::class, 'getbrandsForCategory']);
-    Route::post('/brands', [APIBrandController::class, 'store']);
-    Route::post('/brand-categories', [APIBrandController::class, 'storeBrandCategory']);
+Route::get('/brands', [APIBrandController::class, 'index']);
+Route::get('/brands/{id}', [APIBrandController::class, 'show']);
+Route::get('/brands/isFeatured', [APIBrandController::class, 'featured']);
+Route::get('/brands/category/{categoryId}', [APIBrandController::class, 'getbrandsForCategory']);
+Route::post('/brands', [APIBrandController::class, 'store']);
+Route::post('/brand-categories', [APIBrandController::class, 'storeBrandCategory']);
 
-    Route::get('/products', [APIProductController::class, 'index']);
-    Route::get('/products/{id}', [APIProductController::class, 'show']);
-    Route::post('/products', [APIProductController::class, 'store']);
-    Route::patch('/products/{id}', [APIProductController::class, 'updateSingleField']);
-    Route::put('/products/{id}', [APIProductController::class, 'update']);
-    Route::put('/products/{id}', [APIProductController::class, 'update']);
-    Route::post('/upload', [APIProductController::class, 'uploadFile']);
-    Route::post('/product-categories', [APIProductController::class, 'storeProductCategory']);
+Route::get('/products', [APIProductController::class, 'index']);
+Route::get('/products/{id}', [APIProductController::class, 'show']);
+Route::post('/products', [APIProductController::class, 'store']);
+Route::patch('/products/{id}', [APIProductController::class, 'updateSingleField']);
+Route::put('/products/{id}', [APIProductController::class, 'update']);
+Route::post('/upload', [APIProductController::class, 'uploadFile']);
+Route::post('/product-categories', [APIProductController::class, 'storeProductCategory']);
 
+Route::get('/product-reviews', [APIProductReviewController::class, 'index']);
+Route::post('/product-reviews', [APIProductReviewController::class, 'store']);
+Route::put('/product-reviews/{id}/company-comment', [APIProductReviewController::class, 'addCompanyComment']);
+Route::put('/product-reviews/{id}', [APIProductReviewController::class, 'update']);
+Route::delete('/product-reviews/{id}', [APIProductReviewController::class, 'destroy']);
 
-    Route::get('/product-reviews', [APIProductReviewController::class, 'index']);
-    Route::post('/product-reviews', [APIProductReviewController::class, 'store']);
-    Route::put('/product-reviews/{id}/company-comment', [APIProductReviewController::class, 'addCompanyComment']);
-    Route::put('/product-reviews/{id}', [APIProductReviewController::class, 'update']);
-    Route::delete('/product-reviews/{id}', [APIProductReviewController::class, 'destroy']);
+Route::get('/banners', [APIBannerController::class, 'index']);
+Route::post('/banners', [APIBannerController::class, 'store']);
 
+// Privacy Policy Route
+Route::get('/privacy-policy', [PrivacyPolicyController::class, 'show']);
 
+// Authenticated Routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [APIAuthController::class, 'logout']);
+    Route::post('/email/verification-notification', [APIAuthController::class, 'sendEmailVerificationNotification']);
+    
+    // User Routes
+    Route::get('/user', [APIUserController::class, 'show']);
+    Route::put('/user', [APIUserController::class, 'update']);
+    Route::patch('/user', [APIUserController::class, 'updateField']);
+    Route::post('/user/profile-picture', [APIUserController::class, 'uploadProfilePicture']);
+    Route::delete('/user', [APIUserController::class, 'destroy']);
 
-    Route::get('/banners', [APIBannerController::class, 'index']);
-    Route::post('/banners', [APIBannerController::class, 'store']);
-    // Route::post('/upload', [APIUploadController::class, 'upload']);
+    // Settings Routes
+    Route::get('/settings', [APISettingsController::class, 'show']);
+    Route::post('/settings', [APISettingsController::class, 'store']);
+    Route::put('/settings', [APISettingsController::class, 'update']);
+    Route::patch('/settings', [APISettingsController::class, 'updateField']);
+    Route::get('/settings/global', [APISettingsController::class, 'global']);
 
-    // Authenticated Routes
-    Route::middleware('auth:sanctum')->group(function () {
-            Route::post('/logout', [APIAuthController::class, 'logout']);
-            Route::post('/email/verification-notification', [APIAuthController::class, 'sendEmailVerificationNotification']);
-            
-            // User Routes
-            Route::get('/user', [APIUserController::class, 'show']);
-            Route::put('/user', [APIUserController::class, 'update']);
-            Route::patch('/user', [APIUserController::class, 'updateField']);
-            Route::post('/user/profile-picture', [APIUserController::class, 'uploadProfilePicture']);
-            Route::delete('/user', [APIUserController::class, 'destroy']);
-
-            // Settings Routes
-            Route::get('/settings', [APISettingsController::class, 'show']);
-            Route::post('/settings', [APISettingsController::class, 'store']);
-            Route::put('/settings', [APISettingsController::class, 'update']);
-            Route::patch('/settings', [APISettingsController::class, 'updateField']);
-
-            Route::get('/settings/global', [APISettingsController::class, 'global']);
-
-            // Address Routes
-            //Route::apiResource('addresses', APIAddressController::class);
-
-            // Order Routes
-            Route::get('/orders', [APIOrderController::class, 'index']);
-            Route::post('/orders', [APIOrderController::class, 'store']);
-            Route::get('/orders/{id}', [APIOrderController::class, 'show']);
-            Route::put('/orders/{id}', [APIOrderController::class, 'update']);
-            Route::delete('/orders/{id}', [APIOrderController::class, 'delete']);
-           
-        
-            Route::get('/addresses', [APIAddressController::class, 'index']);
-            Route::post('/addresses', [APIAddressController::class, 'store']);
-            Route::put('/addresses/{id}', [APIAddressController::class, 'update']);
-            Route::patch('/addresses/{id}', [APIAddressController::class, 'patch']);
-            Route::delete('/addresses/{id}', [APIAddressController::class, 'destroy']);
-     
+    // Order Routes
+    Route::get('/orders', [APIOrderController::class, 'index']);
+    Route::post('/orders', [APIOrderController::class, 'store']);
+    Route::get('/orders/{id}', [APIOrderController::class, 'show']);
+    Route::put('/orders/{id}', [APIOrderController::class, 'update']);
+    Route::delete('/orders/{id}', [APIOrderController::class, 'delete']);
+    
+    Route::get('/addresses', [APIAddressController::class, 'index']);
+    Route::post('/addresses', [APIAddressController::class, 'store']);
+    Route::put('/addresses/{id}', [APIAddressController::class, 'update']);
+    Route::patch('/addresses/{id}', [APIAddressController::class, 'patch']);
+    Route::delete('/addresses/{id}', [APIAddressController::class, 'destroy']);
 });
 
 // CSRF Token Endpoint
 Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show'])->name('sanctum.csrf-cookie');
+?>
