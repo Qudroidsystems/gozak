3e56079f948dd8934160b14cd0b329cf6cbbb63c
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-10-23 18:18:50 +0100

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -50,15 +50,16 @@ class LoginController extends Controller
     protected function authenticated(Request $request, $user)
     {
         // Check if the user has 'staff' in the role column
-        if ($user->role !== 'staff') {
-            // Log out the user immediately
-            auth()->logout();
+        // if ($user->role !== 'staff') {
+        //     // Log out the user immediately
+        //     auth()->logout();
 
-            // Throw validation exception to mimic login failure
-            throw ValidationException::withMessages([
-                $this->username() => [trans('auth.failed_staff_role')],
-            ]);
-        }
+        //     // Throw validation exception to mimic login failure
+        //     throw ValidationException::withMessages([
+        //         $this->username() => [trans('auth.failed_staff_role')],
+        //     ]);
+        // }
+        echo $user->name;
 
         // If role check passes, proceed to default redirect
         return redirect()->intended($this->redirectPath());
