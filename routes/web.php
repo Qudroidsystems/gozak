106bf45a188c02fc48cb55941be0566ed80c7339
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-10-24 15:31:59 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,19 +44,11 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/users/paginate', [UserController::class, 'paginate'])->name('users.paginate');
     Route::get('/users/roles', [UserController::class, 'roles'])->name('users.roles');
 
-    // Student-Specific User Creation
-    Route::name('users.')->group(function () {
-        Route::get('add-student', [UserController::class, 'createFromStudentForm'])->name('add-student-form');
-        Route::post('create-from-student', [UserController::class, 'createFromStudent'])->name('create-from-student');
-    });
-
-    // AJAX Endpoints
-    Route::get('/users/students', [UserController::class, 'getStudents'])->name('users.students'); // Renamed from 'get.students' for consistency
-
+ 
     // Biodata Resource
     Route::resource('biodata', BiodataController::class);
 
     // User Overview & Settings
-    Route::get('/users/{user}/overview', [OverviewController::class, 'show'])->name('users.overview');
-    Route::get('/users/{user}/settings', [BiodataController::class, 'show'])->name('users.settings'); // Assuming {id} is user
+    Route::get('/users/{user}/overview', [OverviewController::class, 'show'])->name('user.overview');
+    Route::get('/users/{user}/settings', [BiodataController::class, 'show'])->name('user.settings'); // Assuming {id} is user
 });
