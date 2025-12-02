a0c1f05aa3d35ba37d983f809c91014e9b1fa049
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-12-02 12:55:30 +0100

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -173,6 +173,22 @@
                         </li>
 
                         <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">INVENTORY MANAGEMENT</span></li>
+                         @can('View brand')
+                            <li class="nav-item">
+                                <a class="nav-link menu-link collapsed" href="#sidebarcategories" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarflock">
+                                    <i class="ph-user-circle"></i> <span data-key="t-authentication">Category Managements</span>
+                                </a>
+                                <div class="collapse menu-dropdown" id="sidebarcategories">
+                                    <ul class="nav nav-sm flex-column">
+                                    
+                                            <li class="nav-item">
+                                            <a href="{{ route('categories.index') }}" class="nav-link" role="button" data-key="t-signin"> Category </a>
+                                        </li>
+                                    
+                                    </ul>
+                                </div>
+                            </li>
+                        @endcan
                         @can('View brand')
                             <li class="nav-item">
                                 <a class="nav-link menu-link collapsed" href="#sidebarbrand" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarflock">
@@ -191,22 +207,7 @@
                         @endcan
 
                        
-                        @can('View brand')
-                            <li class="nav-item">
-                                <a class="nav-link menu-link collapsed" href="#sidebarcategories" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarflock">
-                                    <i class="ph-user-circle"></i> <span data-key="t-authentication">Category Managements</span>
-                                </a>
-                                <div class="collapse menu-dropdown" id="sidebarcategories">
-                                    <ul class="nav nav-sm flex-column">
-                                    
-                                            <li class="nav-item">
-                                            <a href="{{ route('categories.index') }}" class="nav-link" role="button" data-key="t-signin"> Category </a>
-                                        </li>
-                                    
-                                    </ul>
-                                </div>
-                            </li>
-                        @endcan
+                       
                         {{-- @can('View flock')
                               <li class="nav-item">
                             <a class="nav-link menu-link collapsed" href="#sidebarroles" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarPages">
