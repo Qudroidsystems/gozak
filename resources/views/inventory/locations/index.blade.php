37254155fddf1b01f2ba116d9f7a0bd57c428a02
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-12-08 17:57:35 +0100

    Update index.blade.php

--- resources/views/inventory/locations/index.blade.php
+++ resources/views/inventory/locations/index.blade.php
@@ -247,9 +247,7 @@
     </div>
 </div>
 
-@endsection
 
-@section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
