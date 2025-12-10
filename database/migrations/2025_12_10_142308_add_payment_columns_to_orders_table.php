630da8f78aab4a9e33da90d5e642f3da9173ba50
Eliab <35658748+eliabsiji@users.noreply.github.com>
2025-12-10 15:44:53 +0100

    Update 2025_12_10_142308_add_payment_columns_to_orders_table.php

--- database/migrations/2025_12_10_142308_add_payment_columns_to_orders_table.php
+++ database/migrations/2025_12_10_142308_add_payment_columns_to_orders_table.php
@@ -12,15 +12,15 @@ return new class extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->enum('payment_status', ['paid', 'unpaid', 'pending', 'failed', 'refunded'])
-                ->default('unpaid')
-                ->after('status');
+            // $table->enum('payment_status', ['paid', 'unpaid', 'pending', 'failed', 'refunded'])
+            //     ->default('unpaid')
+            //     ->after('status');
 
-            $table->timestamp('paid_at')->nullable()->after('payment_status');
-            // $table->string('invoice_number')->nullable()->unique()->after('id');
+            // $table->timestamp('paid_at')->nullable()->after('payment_status');
+            // // $table->string('invoice_number')->nullable()->unique()->after('id');
             
-            $table->index('payment_status');
-            $table->index('status');
+            // $table->index('payment_status');
+            // $table->index('status');
         });
     }
 
